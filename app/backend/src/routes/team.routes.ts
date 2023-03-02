2f76b70e1d6c4797ac29a8a2ac8b1387e77cbfb8
Beterraba <ls6182315@gmail.com>
2023-03-02 16:57:41 -0300

    get team by Id route

--- app/backend/src/routes/team.routes.ts
+++ app/backend/src/routes/team.routes.ts
@@ -8,4 +8,6 @@ const controller = new TeamController();
 
 teamRouter.get('/teams', controller.findAll); // GET /teams
 
+teamRouter.get('/teams/:id', controller.findById); // GET /teams/:id
+
 export default teamRouter;
