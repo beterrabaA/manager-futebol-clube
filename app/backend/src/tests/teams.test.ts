f871c1d58edbc667e84fce77384dd3e6135f2341
Beterraba <ls6182315@gmail.com>
2023-03-02 16:57:12 -0300

    teams tests

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,5 +1,7 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import model from '../database/models/TeamModel';
+import times from './mocks/times.mock';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
@@ -12,7 +14,16 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
+describe('Verifica-se o fluxo de Times', () => {
+  describe('Testando a camada Service', () => {
+    it('Deve retornar um array de times', async () => {
+      const stub = sinon.stub(model, 'findAll').resolves(times);
+      const result = await model.findAll();
+      expect(result).to.be.an('array');
+      expect(result).to.have.lengthOf(2);
+      stub.restore();
+    }
+  });
   /**
    * Exemplo do uso de stubs com tipos
    */
