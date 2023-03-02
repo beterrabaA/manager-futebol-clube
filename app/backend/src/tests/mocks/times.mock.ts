74268f66fccf1867594b8f120adbc5bd8839ad4d
Beterraba <ls6182315@gmail.com>
2023-03-02 16:57:23 -0300

    team mocks

---
+++ app/backend/src/tests/mocks/times.mock.ts
@@ -0,0 +1,28 @@
+const times = [
+    {
+        id: 1,
+        name: 'Time 1',
+    },
+    {
+        id: 2,
+        name: 'Time 2',
+    },
+    {
+        id: 3,
+        name: 'Time 3',
+    },
+    {
+        id: 4,
+        name: 'Time 4',
+    },
+    {
+        id: 5,
+        name: 'Time 5',
+    },
+    {
+        id: 6,
+        name: 'Time 6',
+    }
+];
+
+export default times;
