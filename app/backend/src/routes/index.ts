5b79231bc3d2abead68c64f72d3e75192c9f82fd
Beterraba <ls6182315@gmail.com>
2023-03-03 19:02:28 -0300

    add user routes

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import teamRouter from './team.routes';
+import userRouter from './user.routes';
 
 const router = Router();
 
 router.use(teamRouter);
+
+router.use(userRouter);
 
 export default router;
