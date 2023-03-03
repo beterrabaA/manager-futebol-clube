b052efa09bf5c8b6eadbe5d847f4384eadb16caa
Lucas Vieira <ls6182315@gmail.com>
2023-03-03 11:37:07 +0000

    team path tests

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,6 +1,6 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
-import model from '../database/models/TeamModel';
+import Team from '../database/models/TeamModel';
 import times from './mocks/times.mock';
 // @ts-ignore
 import chaiHttp = require('chai-http');
@@ -15,42 +15,35 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Verifica-se o fluxo de Times', () => {
-  describe('Testando a camada Service', () => {
-    it('Deve retornar um array de times', async () => {
-      const stub = sinon.stub(model, 'findAll').resolves(times);
-      const result = await model.findAll();
-      expect(result).to.be.an('array');
-      expect(result).to.have.lengthOf(2);
-      stub.restore();
-    }
+  let chaiHttpResponse: Response;
+
+  beforeEach(async () => {
+    sinon
+      .stub(Team, "findAll")
+      .resolves(times as unknown as Team[]);
   });
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
 
-  // let chaiHttpResponse: Response;
-
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
-
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
-
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+  
+  it('Verifica a resposta de /teams', async () => {
+    
+    chaiHttpResponse = await chai.request(app).get('/teams');
+    
+    expect(chaiHttpResponse.status).to.be.equal(200);
   });
+
+  afterEach(() => {
+    (Team.findAll as sinon.SinonStub).restore();
+  });
+
+  it('Verifica a resposta de um time específico', async () => {
+
+    sinon
+    .stub(Team, "findByPk")
+    .resolves(times[2] as unknown as Team);
+
+    chaiHttpResponse = await chai.request(app).get('/teams/2')
+
+    expect(chaiHttpResponse.status).to.be.equal(200);
+
+  })
 });
