0b7ef49302e786eebda969a4d470c428c694987b
Beterraba <ls6182315@gmail.com>
2023-03-03 23:36:52 -0300

    login service

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -1,6 +1,9 @@
 import { ModelStatic } from 'sequelize';
+import * as bcrypt from 'bcryptjs';
+import { tokenGenerator } from '../utils/token';
 import Users from '../database/models/UserModel';
 import TypeMsg from '../interfaces/resService.interface';
+import validateLogin from './validations/validationsInputsValues';
 
 export default class UserService {
   public model: ModelStatic<Users>;
@@ -9,15 +12,26 @@ export default class UserService {
     this.model = Users;
   }
 
-  public async getUser(email: string, _password: string): Promise<TypeMsg> {
+  public async getUser(email: string, password: string): Promise<TypeMsg> {
+    const error = validateLogin(email, password);
+
+    if (error.type !== 'NULL') return error;
+
     const users = await this.model.findOne(
       { where: { email } },
     );
 
-    console.log(users);
+    const vPass = bcrypt.compareSync(password, users?.dataValues.password);
 
-    if (!users) return { type: 'invalidToken', message: 'Invalid email or password' };
+    console.log(users?.dataValues);
+    if (!users || !vPass) {
+      return { type: 'invalidToken',
+        message: {
+          message: 'Invalid email or password' } };
+    }
 
-    return { type: 'success', message: users };
+    const token = tokenGenerator(users?.dataValues);
+
+    return { type: 'success', message: { token } };
   }
 }
