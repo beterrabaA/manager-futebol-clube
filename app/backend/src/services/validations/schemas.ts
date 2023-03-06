d65e6e6e499ca6cf7773f8972b4f70bb0db95eee
Beterraba <ls6182315@gmail.com>
2023-03-05 22:41:00 -0300

    valid email validation

--- app/backend/src/services/validations/schemas.ts
+++ app/backend/src/services/validations/schemas.ts
@@ -1,11 +1,14 @@
 import Joi = require('joi');
 
+const ALL_FIELDS_REQUIRED = 'All fields must be filled';
+
 export const loginSchema = Joi.object({
   email: Joi.string().email().empty().required(),
   password: Joi.string().empty().required(),
 }).messages({
-  'string.empty': 'All fields must be filled',
-  'any.required': 'All fields must be filled',
+  'string.empty': ALL_FIELDS_REQUIRED,
+  'string.email': ALL_FIELDS_REQUIRED,
+  'any.required': ALL_FIELDS_REQUIRED,
 });
 
 export const teamSchema = Joi.object({
