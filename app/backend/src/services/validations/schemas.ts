5cdbc4a6fc945681de186bc0e8633efe2f5801ed
Beterraba <ls6182315@gmail.com>
2023-03-05 23:35:18 -0300

    pass val min

--- app/backend/src/services/validations/schemas.ts
+++ app/backend/src/services/validations/schemas.ts
@@ -4,10 +4,11 @@ const ALL_FIELDS_REQUIRED = 'All fields must be filled';
 
 export const loginSchema = Joi.object({
   email: Joi.string().email().empty().required(),
-  password: Joi.string().empty().required(),
+  password: Joi.string().empty().min(6).required(),
 }).messages({
   'string.empty': ALL_FIELDS_REQUIRED,
   'string.email': 'Invalid email or password',
+  'string.min': 'Invalid email or password',
   'any.required': ALL_FIELDS_REQUIRED,
 });
 
--- app/backend/src/services/validations/validationsInputsValues.ts
+++ app/backend/src/services/validations/validationsInputsValues.ts
@@ -4,7 +4,6 @@ const validateLogin = (email: string, password: string) => {
   const { error } = loginSchema.validate({ email, password });
   const response = error?.message || '';
   const { type } = error?.details[0] || {};
-  console.log(type);
   if (type === 'string.email') return { type: 'invalidToken', message: { message: response } };
   if (error) return { type: 'missingKey', message: { message: response } };
 
