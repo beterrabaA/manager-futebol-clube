c86815726645a40a2bead719f2c5a6bd2fee98b8
Beterraba <ls6182315@gmail.com>
2023-03-05 21:41:10 -0300

    add new msg joi errors

--- app/backend/src/services/validations/schemas.ts
+++ app/backend/src/services/validations/schemas.ts
@@ -1,9 +1,10 @@
 import Joi = require('joi');
 
 export const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
+  email: Joi.string().email().empty().required(),
   password: Joi.string().required(),
 }).messages({
+  'any.empty': 'All fields must be filled',
   'any.required': 'All fields must be filled',
 });
 
