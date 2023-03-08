982b9d4844117920e70c7bb888c26f3d9dc284aa
Beterraba <ls6182315@gmail.com>
2023-03-07 21:39:32 -0300

    login-role router e midlleware validation

--- app/backend/src/routes/user.routes.ts
+++ app/backend/src/routes/user.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
 import UserController from '../controllers/UserController';
+import Midlleware from '../auth/validateJwT';
 
 const userRouter = Router();
 
 const controller = new UserController();
 
-userRouter.post('/login', controller.login); // POST /login (login)
+const { validateJWT } = new Midlleware();
+
+userRouter.post('/login', validateJWT, controller.login); // POST /login (login)
+
+userRouter.get('/login/role', validateJWT, controller.role); // GET /role (role)
 
 export default userRouter;
