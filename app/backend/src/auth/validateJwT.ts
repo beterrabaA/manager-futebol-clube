6161d9d7c5f5a0ba854ed1cf88a7d115e996be86
Beterraba <ls6182315@gmail.com>
2023-03-07 21:39:44 -0300

    validation midlleware

--- app/backend/src/auth/validateJwT.ts
+++ app/backend/src/auth/validateJwT.ts
@@ -1,9 +1,11 @@
-import { Request, Response, NextFunction } from 'express';
-import { verify } from 'jsonwebtoken';
+import { Response, NextFunction } from 'express';
 
 import UserService from '../services/UserService';
+import { tokenDecoder } from '../utils/token';
+import Iuser from '../interfaces/user.interface';
+import CustomRequest from '../interfaces/customReq.interface';
 
-const { JWT_SECRET } = process.env as { [key: string]: string };
+// const secret = process.env.JWT_SECRET as string;
 
 class Midlleware {
   private service: UserService;
@@ -12,14 +14,17 @@ class Midlleware {
     this.service = new UserService();
   }
 
-  public validateJWT = (req: Request, res: Response, next: NextFunction) => {
+  public validateJWT = async (req: CustomRequest, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
     if (!token) return res.status(401).json({ message: 'Token not found' });
 
     try {
-      const decoded = verify(token, JWT_SECRET) as { email: string };
+      const decoded = tokenDecoder(token) as Iuser;
+      console.log(decoded);
       const user = this.service.getUserByEmail(decoded.email);
       if (!user) return res.status(401).json({ message: 'Token must be a valid token' });
+
+      req.role = decoded.role;
       next();
     } catch (error) {
       return res.status(401).json({ message: 'Expired or invalid token' });
@@ -27,17 +32,17 @@ class Midlleware {
   };
 }
 
-const validateJWT = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization;
-  if (!token) return res.status(401).json({ message: 'Token not found' });
+// const validateJWT = (req: Request, res: Response, next: NextFunction) => {
+//   const token = req.headers.authorization;
+//   if (!token) return res.status(401).json({ message: 'Token not found' });
 
-  try {
-    const decoded = verify(token, JWT_SECRET) as { email: string };
-    const user = service.getUserByEmail(decoded.email);
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: 'Expired or invalid token' });
-  }
-};
+//   try {
+//     const decoded = verify(token, JWT_SECRET) as { email: string };
+//     const user = service.getUserByEmail(decoded.email);
+//     next();
+//   } catch (error) {
+//     return res.status(401).json({ message: 'Expired or invalid token' });
+//   }
+// };
 
-export default validateJWT;
+export default Midlleware;
