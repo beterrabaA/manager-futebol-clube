7a7efc78decec079be37843a84cc34ffbfab68c9
Lucas Vieira <ls6182315@gmail.com>
2023-03-12 18:34:23 -0300

    create match service

---
+++ app/backend/src/services/MatchService.ts
@@ -0,0 +1,22 @@
+import { ModelStatic } from 'sequelize';
+import Teams from '../database/models/TeamModel';
+import Matches from '../database/models/MatchModel';
+
+export default class MatchService {
+  private model: ModelStatic<Matches>;
+
+  constructor() {
+    this.model = Matches;
+  }
+
+  public async getAll() {
+    const matches = await this.model.findAll({
+      include: [
+        { model: Teams, as: 'homeTeam', attributes: { exclude: ['id'] } },
+        { model: Teams, as: 'awayTeam', attributes: { exclude: ['id'] } },
+      ],
+    });
+
+    return { type: 'success', message: matches };
+  }
+}
