a92a41a0454af040aeaf2d5e996160cceb295782
Lucas Vieira <ls6182315@gmail.com>
2023-03-12 18:33:50 -0300

    add match router

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import teamRouter from './team.routes';
 import userRouter from './user.routes';
+import matchRouter from './match.routes';
 
 const router = Router();
 
 router.use(teamRouter);
 
 router.use(userRouter);
+
+router.use(matchRouter);
 
 export default router;
