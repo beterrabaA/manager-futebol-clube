9ba69141cb570c7efa7f2555fca6ef6bf8994245
Lucas Vieira <ls6182315@gmail.com>
2023-03-12 22:00:43 +0000

    fix lint

--- app/backend/src/database/models/MatchModel.ts
+++ app/backend/src/database/models/MatchModel.ts
@@ -3,60 +3,59 @@ import db from '.';
 import Teams from './TeamModel';
 
 class Matches extends Model {
-    declare readonly id: number;
-    declare homeTeamId: number;
-    declare homeTeamGoals: number;
-    declare awayTeamId: number;
-    declare awayTeamGoals: number;
-    declare inProgress: boolean;
+  declare readonly id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
 }
 
 Matches.init(
-    {
-        id: {
-            type: INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        homeTeamId: {
-            type: INTEGER,
-            references: {
-                model: 'teams',
-                key: 'id',
-            },
-        },
-        homeTeamGoals: {
-            type: INTEGER,
-            allowNull: false,
-        },
-        awayTeamId: {
-            type: INTEGER,
-            references: {
-                model: 'teams',
-                key: 'id',
-            },
-        },
-        awayTeamGoals: {
-            type: INTEGER,
-            allowNull: false,
-        },
-        inProgress: {
-            type: BOOLEAN,
-            allowNull: false,
-        },
-    }
-    , {
-        sequelize: db,
-        modelName: 'Match',
-        tableName: 'matches',
-        timestamps: false,
-        underscored: true,
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-)
+    homeTeamId: {
+      type: INTEGER,
+      references: {
+        model: 'teams',
+        key: 'id',
+      },
+    },
+    homeTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    awayTeamId: {
+      type: INTEGER,
+      references: {
+        model: 'teams',
+        key: 'id',
+      },
+    },
+    awayTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    inProgress: {
+      type: BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'Match',
+    tableName: 'matches',
+    timestamps: false,
+    underscored: true,
+  },
+);
 
 Matches.belongsTo(Teams, { foreignKey: 'homeTeamId', as: 'homeTeam' });
 Matches.belongsTo(Teams, { foreignKey: 'awayTeamId', as: 'awayTeam' });
-
 
 export default Matches;
