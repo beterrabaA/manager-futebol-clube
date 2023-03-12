67788fbf734988967c4aef4c90c4451647d88fc2
Lucas Vieira <ls6182315@gmail.com>
2023-03-12 19:40:05 +0000

    match model

---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,63 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Example from './ExampleModel';
+import Teams from './TeamModel';
+
+class Matches extends Model {
+    declare readonly id: number;
+    declare homeTeamId: number;
+    declare homeTeamGoals: number;
+    declare awayTeamId: number;
+    declare awayTeamGoals: number;
+    declare inProgress: boolean;
+}
+
+Matches.init(
+    {
+        id: {
+            type: INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        homeTeamId: {
+            type: INTEGER,
+            references: {
+                model: 'teams',
+                key: 'id',
+            },
+        },
+        homeTeamGoals: {
+            type: INTEGER,
+            allowNull: false,
+        },
+        awayTeamId: {
+            type: INTEGER,
+            references: {
+                model: 'teams',
+                key: 'id',
+            },
+        },
+        awayTeamGoals: {
+            type: INTEGER,
+            allowNull: false,
+        },
+        inProgress: {
+            type: BOOLEAN,
+            allowNull: false,
+        },
+    }
+    , {
+        sequelize: db,
+        modelName: 'Match',
+        tableName: 'matches',
+        timestamps: false,
+        underscored: true,
+    },
+)
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeamId', as: 'awayTeam' });
+
+
+export default Matches;
