61c8b39991e935849d361facb247c2dda8c17efb
Lucas Vieira <ls6182315@gmail.com>
2023-03-12 21:06:17 -0300

    add query in matches

--- app/backend/src/controllers/MatchController .ts
+++ app/backend/src/controllers/MatchController .ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import MatchService from '../services/MatchService';
 import mapError from './errorMap';
+import convertToBoolean from '../utils/boolean';
 
 class MatchController {
   private service: MatchService;
@@ -10,17 +11,18 @@ class MatchController {
   }
 
   public findAll = async (req: Request, res: Response) => {
+    const search = req.query;
+    const conValue = convertToBoolean(search?.inProgress as string);
+
+    if (conValue !== undefined) {
+      const { type, message } = await this.service.query(conValue);
+      return res.status(mapError(type)).json(message);
+    }
+
     const { type, message } = await this.service.getAll();
 
     return res.status(mapError(type)).json(message);
   };
-
-  //   public findById = async (req: Request, res: Response) => {
-  //     const { id } = req.params;
-  //     const { type, message } = await this.service.getById(Number(id));
-
-//     return res.status(mapError(type)).json(message);
-//   };
 }
 
 export default MatchController;
