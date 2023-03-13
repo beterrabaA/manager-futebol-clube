f9fc7c134c9b5a14b513e0097eec83aab1396efa
Lucas Vieira <ls6182315@gmail.com>
2023-03-12 21:08:09 -0300

    finish match service

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -31,4 +31,20 @@ export default class MatchService {
 
     return { type: 'success', message: matches };
   }
+
+  public async finishMatch(id: number) {
+    const match = await this.model.findByPk(id);
+
+    if (!match) {
+      return { type: 'notFound', message: 'Match not found' };
+    }
+
+    if (match.inProgress) {
+      match.inProgress = false;
+      await match.save();
+      return { type: 'success', message: 'Finished' };
+    }
+
+    return { type: 'badRequest', message: 'Match already finished' };
+  }
 }
