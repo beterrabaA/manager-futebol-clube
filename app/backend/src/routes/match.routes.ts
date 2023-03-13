5152c8f8a1a1e2d5229fea97a5f6c7563167c51f
Lucas Vieira <ls6182315@gmail.com>
2023-03-12 21:05:00 -0300

    token validator /match

--- app/backend/src/routes/match.routes.ts
+++ app/backend/src/routes/match.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 
 import MatchController from '../controllers/MatchController ';
+import Midlleware from '../auth/validateJwT';
 
 const matchRouter = Router();
 
 const matController = new MatchController();
+const { validateJWT } = new Midlleware();
 
 matchRouter.get('/matches', matController.findAll); // GET /teams
 
-matchRouter.patch('/matches/:id/finish', matController.finish); // PATCH /teams/:id/finish
+matchRouter.patch('/matches/:id/finish', validateJWT, matController.finish); // PATCH /teams/:id/finish
 
 export default matchRouter;
