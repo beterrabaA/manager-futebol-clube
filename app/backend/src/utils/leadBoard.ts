f1c28bf53f358f2dd7da39488562995ecb474176
Lucas Vieira <ls6182315@gmail.com>
2023-03-16 20:19:39 -0300

    leadboard sort function

---
+++ app/backend/src/utils/leadBoard.ts
@@ -0,0 +1,13 @@
+import listLead from '../interfaces/listLead.interface';
+
+const sortList = (list: listLead[]) =>
+  list.sort((a, b) => {
+    if (b.totalPoints !== a.totalPoints) return b.totalPoints - a.totalPoints;
+    if (b.totalVictories !== a.totalVictories) return b.totalVictories - a.totalVictories;
+    if (b.goalsBalance !== a.goalsBalance) return b.goalsBalance - a.goalsBalance;
+    if (b.goalsFavor !== a.goalsFavor) return b.goalsFavor - a.goalsFavor;
+    return a.goalsOwn - b.goalsOwn;
+  });
+
+
+export default sortList;
