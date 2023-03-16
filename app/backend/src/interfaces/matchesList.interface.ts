ee49bbb355c5460ff1e1596dfe2b77624728d0a2
Lucas Vieira <ls6182315@gmail.com>
2023-03-16 20:20:50 -0300

    matches list interface

---
+++ app/backend/src/interfaces/matchesList.interface.ts
@@ -0,0 +1,16 @@
+interface MatchLst {
+  id: number,
+  homeTeamId: number,
+  awayTeamId: number,
+  homeTeamGoals: number,
+  awayTeamGoals: number,
+  inProgress: boolean,
+  homeTeam: {
+    teamName: string
+  },
+  awayTeam: {
+    teamName: string
+  }
+}
+
+export default MatchLst;
