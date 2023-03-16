00997f2995bf58ed36e78194e90d80ec2ddf3aa8
Lucas Vieira <ls6182315@gmail.com>
2023-03-16 20:14:15 -0300

    add leadboard router

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import teamRouter from './team.routes';
 import userRouter from './user.routes';
 import matchRouter from './match.routes';
+import leadRouter from './lead.routes';
 
 const router = Router();
 
@@ -10,5 +11,7 @@ router.use(teamRouter);
 router.use(userRouter);
 
 router.use(matchRouter);
+
+router.use(leadRouter);
 
 export default router;
