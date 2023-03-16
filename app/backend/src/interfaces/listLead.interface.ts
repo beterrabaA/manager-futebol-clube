6a35b1ff136b6458ec8e28865b60c92e2cdd81b4
Lucas Vieira <ls6182315@gmail.com>
2023-03-16 20:13:38 -0300

    listBoard interface

---
+++ app/backend/src/interfaces/listLead.interface.ts
@@ -0,0 +1,14 @@
+interface listLead {
+  name: string;
+  totalPoints: number;
+  totalGames: number;
+  totalVictories: number;
+  totalDraws: number;
+  totalLosses: number;
+  goalsFavor: number;
+  goalsOwn: number;
+  goalsBalance: number;
+  efficiency: string;
+}
+
+export default listLead;
